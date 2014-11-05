7b756aea560bab0ac296418841e34ad47d7318eb
DomCristaldi <domeniccristaldi@gmail.com>
2014-11-05 14:42:48 -0500

    gui doesn't wanna work. gonna keep trying

---
+++ TranscendGame/Assets/Scripts/DialogSelectionScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DialogSelectionScript : MonoBehaviour {
+
+	public GameObject [] CanvasArray;
+
+	// Use this for initialization
+	void Start () {
+		CanvasArray = new GameObject[GetComponent<Transform>().childCount];
+
+		int i = 0;
+		foreach(Transform child in GetComponent<Transform>() ) {
+			if (child.tag != "notCanvas") {
+				CanvasArray[i] = child.gameObject;
+
+				++i;
+			}
+		}
+
+		foreach (GameObject thing in CanvasArray) {
+			Debug.Log(thing.tag);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TranscendGame/Assets/Scripts/PlayerTextSelectorScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTextSelectorScript : MonoBehaviour {
+
+	public Canvas MasterCanvas;
+	public int indexNum;
+
+	string LookForCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (indexNum != null) {
+			if (indexNum == 0) {
+				LookForCanvas = "Canvas0";
+			}
+			if (indexNum == 1) {
+				LookForCanvas = "Canvas1";
+			}
+			if (indexNum == 2) {
+				LookForCanvas = "Canvas2";
+			}
+			if (indexNum == 3) {
+				LookForCanvas = "Canvas3";
+			}
+			if (indexNum == 4) {
+				LookForCanvas = "Canvas4";
+			}
+			if (indexNum == 5) {
+				LookForCanvas = "Canvas5";
+			}
+			if (indexNum == 6) {
+				LookForCanvas = "Canvas6";
+			}
+			if (indexNum == 7) {
+				LookForCanvas = "Canvas7";
+			}
+		}
+
+		foreach (GameObject cnvs in MasterCanvas.GetComponent<DialogSelectionScript>().CanvasArray) {
+			//Debug.Log(cnvs.tag);
+			if (cnvs != null) {
+
+				if (cnvs.tag == LookForCanvas) {
+					cnvs.GetComponent<Canvas>().enabled = true;
+				}
+				else {
+
+					cnvs.GetComponent<Canvas>().enabled = false;
+				}
+			}
+		}
+	}
+}
