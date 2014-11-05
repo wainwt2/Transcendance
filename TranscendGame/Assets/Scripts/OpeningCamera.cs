1f26efd7d9dc13bb68f6b5424627425164d6bf41
Justin Cirigliano <cirigj@rpi.edu>
2014-11-05 13:12:17 -0500

    Some folders are randomly getting moved around for whatever reason. Anyway, basic opening is working.

---
+++ TranscendGame/Assets/Scripts/OpeningCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningCamera : MonoBehaviour {
+
+	private Vector3 kiteString;
+	private Vector3 startPos;
+	private float startEulerX;
+	private float startEulerY;
+	private float startEulerZ;
+	private Transform tf;
+	private Transform playerTf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform>();
+		playerTf = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
+		kiteString = tf.position - playerTf.position;
+		startPos = tf.position;
+		startEulerX = tf.rotation.eulerAngles.x;
+		startEulerY = tf.rotation.eulerAngles.y;
+		startEulerZ = tf.rotation.eulerAngles.z;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		tf.position = playerTf.position + kiteString;
+		tf.rotation = Quaternion.Euler(-Vector3.SqrMagnitude(tf.position - startPos) * .030f + startEulerX, startEulerY, startEulerZ);
+	}
+}
